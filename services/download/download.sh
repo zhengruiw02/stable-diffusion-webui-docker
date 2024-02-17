40f93cd7d4f8f23ebddd7b769a9b62e82b9752b8
Henry Zheng <zhengruiw02@gmail.com>
2024-02-17 12:44:01 +0800

    modify to use mirror source for huggingface.co

--- services/download/download.sh
+++ services/download/download.sh
@@ -15,7 +15,7 @@ mkdir -vp /data/.cache \
 
 echo "Downloading, this might take a while..."
 
-aria2c -x 10 --disable-ipv6 --input-file /docker/links.txt --dir /data/models --continue
+aria2c -x 16 --input-file /docker/links.txt --dir /data/models --continue
 
 echo "Checking SHAs..."
 
